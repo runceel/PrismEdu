5b29abca0be0b9515facaaa46e824a4eceb9cfce
Kazuki Ohta <k_ota28@hotmail.com>
2015-09-19 07:51:20 +0900

    追加忘れファイルを追加

---
+++ 05.InteractionRequest/InteractionRequestSampleModule/Models/InputNotification.cs
@@ -0,0 +1,9 @@
+﻿using Prism.Interactivity.InteractionRequest;
+
+namespace InteractionRequestSampleModule.Models
+{
+    class InputNotification : Notification
+    {
+        public string InputText { get; set; }
+    }
+}
---
+++ 05.InteractionRequest/InteractionRequestSampleModule/ViewModels/InputViewModel.cs
@@ -0,0 +1,46 @@
+﻿using InteractionRequestSampleModule.Models;
+using Prism.Commands;
+using Prism.Interactivity.InteractionRequest;
+using Prism.Mvvm;
+using System;
+
+namespace InteractionRequestSampleModule.ViewModels
+{
+    class InputViewModel : BindableBase, IInteractionRequestAware
+    {
+        // IInteractionRequestAware
+        public Action FinishInteraction { get; set; }
+        private INotification notification;
+        public INotification Notification
+        {
+            get { return this.notification; }
+            set
+            {
+                this.notification = value;
+                // initialize
+                this.InputText = "";
+            }
+        }
+
+        private string inputText;
+
+        public string InputText
+        {
+            get { return this.inputText; }
+            set { this.SetProperty(ref this.inputText, value); }
+        }
+
+        public DelegateCommand OKCommand { get; }
+
+        public InputViewModel()
+        {
+            this.OKCommand = new DelegateCommand(() =>
+                {
+                    ((InputNotification)this.Notification).InputText = this.InputText;
+                    this.FinishInteraction();
+                },
+                () => !string.IsNullOrWhiteSpace(this.InputText))
+                .ObservesProperty(() => this.InputText);
+        }
+    }
+}
