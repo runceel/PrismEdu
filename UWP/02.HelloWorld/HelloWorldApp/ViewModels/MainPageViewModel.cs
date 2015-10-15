5565840f7de6224e7b35e7362a1e536f2c954494
Kazuki Ohta <k_ota28@hotmail.com>
2015-10-15 22:11:01 +0900

    Hello world

---
+++ UWP/02.HelloWorld/HelloWorldApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Commands;
+using Prism.Windows.Mvvm;
+
+namespace HelloWorldApp.ViewModels
+{
+    class MainPageViewModel : ViewModelBase
+    {
+        private string input;
+
+        public string Input
+        {
+            get { return this.input; }
+            set { this.SetProperty(ref this.input, value); }
+        }
+
+        private string output;
+
+        public string Output
+        {
+            get { return this.output; }
+            set { this.SetProperty(ref this.output, value); }
+        }
+
+        public DelegateCommand CreateOutputCommand { get; }
+
+        public MainPageViewModel()
+        {
+            this.CreateOutputCommand = new DelegateCommand(() =>
+                {
+                    this.Output = $"{Input}が入力されました";
+                },
+                () => !string.IsNullOrWhiteSpace(this.Input))
+                .ObservesProperty(() => this.Input);
+        }
+    }
+}
